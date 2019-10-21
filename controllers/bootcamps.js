880ca8c0814f9894562ccd970833292e43925906
Paulius_Juknis <paulius.juknis@gmail.com>
2019-10-21 16:48:19 +0300

    advanced filtering with mongo and query params for getBootcamps route

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -4,7 +4,14 @@ const geocoder = require('../utils/geocoder');
 const Bootcamp = require('../models/Bootcamp');
 
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
-  const bootcamps = await Bootcamp.find();
+  let query;
+
+  let queryStr = JSON.stringify(req.query);
+  queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);
+
+  query = Bootcamp.find(JSON.parse(queryStr));
+
+  const bootcamps = await query;
   res.status(200).json({
     success: true,
     count: bootcamps.length,
