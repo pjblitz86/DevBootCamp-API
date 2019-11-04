ef3cdab1dfca8764ab91f564fbb1c33c24a63ad0
Paulius_Juknis <paulius.juknis@gmail.com>
2019-11-04 10:03:45 +0200

    add pagination

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -6,7 +6,7 @@ const Bootcamp = require('../models/Bootcamp');
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
   let query;
   const reqQuery = { ...req.query };
-  const removeFields = ['select', 'sort'];
+  const removeFields = ['select', 'sort', 'page', 'limit'];
 
   removeFields.forEach(param => delete reqQuery[param]);
 
@@ -29,10 +29,38 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
     query = query.sort('-createdAt');
   }
 
+  // Pagination
+  const page = parseInt(req.query.page, 10) || 1;
+  const limit = parseInt(req.query.limit, 10) || 25;
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
+  const total = await Bootcamp.find(JSON.parse(queryStr)).countDocuments();
+
+  query = query.skip(startIndex).limit(limit);
+
   const bootcamps = await query;
+
+  // Pagination result
+  const pagination = {};
+
+  if (endIndex < total) {
+    pagination.next = {
+      page: page + 1,
+      limit
+    };
+  }
+
+  if (startIndex > 0) {
+    pagination.prev = {
+      page: page - 1,
+      limit
+    };
+  }
+
   res.status(200).json({
     success: true,
     count: bootcamps.length,
+    pagination,
     data: bootcamps
   });
 });
