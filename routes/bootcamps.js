cac0d9ede86dda45f0e13d6a902f67790016b41d
Paulius_Juknis <paulius.juknis@gmail.com>
2019-11-05 16:56:27 +0200

    advanced results middleware

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -9,6 +9,9 @@ const {
   bootcampPhotoUpload
 } = require('../controllers/bootcamps');
 
+const Bootcamp = require('../models/Bootcamp');
+const advancedResults = require('../middleware/advancedResults');
+
 const courseRouter = require('./courses');
 const router = express.Router();
 
@@ -20,7 +23,7 @@ router.route('/:id/photo').put(bootcampPhotoUpload);
 
 router
   .route('/')
-  .get(getBootcamps)
+  .get(advancedResults(Bootcamp, 'courses'), getBootcamps)
   .post(createBootcamp);
 
 router
