2f7afdd92d13991aa0291381f49de2d0d55afa08
Paulius_Juknis <paulius.juknis@gmail.com>
2019-11-06 16:49:40 +0200

    user login

--- controllers/auth.js
+++ controllers/auth.js
@@ -19,3 +19,30 @@ exports.register = asyncHandler(async (req, res, next) => {
     token
   });
 });
+
+exports.login = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return next(new ErrorResponse('Please provide an email and password', 400));
+  }
+
+  const user = await User.findOne({ email }).select('+password');
+
+  if (!user) {
+    return next(new ErrorResponse('Invalid credentials', 401));
+  }
+
+  const isMatch = await user.matchPassword(password);
+
+  if (!isMatch) {
+    return next(new ErrorResponse('Invalid credentials', 401));
+  }
+
+  const token = user.getSignedJwtToken();
+
+  res.status(200).json({
+    success: true,
+    token
+  });
+});
