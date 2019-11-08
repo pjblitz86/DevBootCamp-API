dac9c423b196a11b12284e977b3a3eac98aec054
Paulius_Juknis <paulius.juknis@gmail.com>
2019-11-08 09:11:03 +0200

    Bootcamp and User relationship

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -22,6 +22,18 @@ exports.getBootcamp = asyncHandler(async (req, res, next) => {
 });
 
 exports.createBootcamp = asyncHandler(async (req, res, next) => {
+  req.body.user = req.user.id;
+  const publishedBootcamp = await Bootcamp.findOne({ user: req.user.id });
+
+  if (publishedBootcamp && req.user.role !== 'admin') {
+    return next(
+      new ErrorResponse(
+        `User with id ${req.user.id} has already published a bootcamp`
+      ),
+      400
+    );
+  }
+
   const bootcamp = await Bootcamp.create(req.body);
   res.status(201).json({
     success: true,
--- seeder.js
+++ seeder.js
@@ -7,6 +7,7 @@ dotenv.config({ path: './config/config.env' });
 
 const Bootcamp = require('./models/Bootcamp');
 const Course = require('./models/Course');
+const User = require('./models/User');
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
@@ -23,10 +24,15 @@ const courses = JSON.parse(
   fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8')
 );
 
+const users = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/users.json`, 'utf-8')
+);
+
 const importData = async () => {
   try {
     await Bootcamp.create(bootcamps);
     await Course.create(courses);
+    await User.create(users);
     console.log('Data imported...'.green.inverse);
     process.exit();
   } catch (err) {
@@ -38,6 +44,7 @@ const deleteData = async () => {
   try {
     await Bootcamp.deleteMany();
     await Course.deleteMany();
+    await User.deleteMany();
     console.log('Data destroyed...'.red.inverse);
     process.exit();
   } catch (err) {
