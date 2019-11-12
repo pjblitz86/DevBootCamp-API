c44b2c915b60069e7b76dacdd56d7a326cc55d87
Paulius_Juknis <paulius.juknis@gmail.com>
2019-11-12 13:59:46 +0200

    logout to clear token

--- controllers/auth.js
+++ controllers/auth.js
@@ -48,6 +48,18 @@ exports.getMe = asyncHandler(async (req, res, next) => {
   });
 });
 
+exports.logout = asyncHandler(async (req, res, next) => {
+  res.cookie('token', 'none', {
+    expires: new Date(Date.now() + 10 * 1000),
+    httpOnly: true
+  });
+
+  res.status(200).json({
+    success: true,
+    data: {}
+  });
+});
+
 exports.updateDetails = asyncHandler(async (req, res, next) => {
   const fieldsToUpdate = {
     name: req.body.name,
